6a135eaef6f8d903684e9c11f8f86072da4ec5f6
Pavlo <pavelmk8@gmail.com>
2021-10-14 22:09:35 +0300

    add: implementation of Stack

--- src/st-stack.js
+++ src/st-stack.js
@@ -12,19 +12,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
- module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+module.exports = class Stack {
+    constructor() {
+        this.itemsArray = [];
+    }
+    push(element) {
+        this.itemsArray.push(element);
+    }
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    pop() {
+        if (this.itemsArray.length == 0)
+            return "Underflow";
+        return this.itemsArray.pop();
+    }
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    peek() {
+        return this.itemsArray[this.itemsArray.length - 1];
+    }
 }
